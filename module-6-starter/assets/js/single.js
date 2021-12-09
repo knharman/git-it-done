ecfc2f3c8e37409694e4500471bdf49d785a4618
Waffles <knharman@gmail.com>
2021-12-08 17:33:38 -0800

    search by different programming languages

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,7 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
-var queryString = document.location.search;
 
 var getRepoName = function () {
     // grab repo name from url query string
@@ -52,6 +51,7 @@ var displayIssues = function (issues) {
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
+        
         // create span to hold issue title
         var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
@@ -79,6 +79,7 @@ var displayIssues = function (issues) {
 var displayWarning = function (repo) {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
     var linkEl = document.createElement("a");
     linkEl.textContent = "See More Issues on GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
@@ -88,5 +89,5 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues();
+// getRepoIssues();
 getRepoName();
